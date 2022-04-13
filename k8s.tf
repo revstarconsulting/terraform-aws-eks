98edcc9fcbd2d62e2583f85caab4003f997e8873
Balkaran Brar <balkaran@cloudprodigy.ca>
2022-04-13 14:35:16 -0400

    added k8s aws-auth map

---
+++ k8s.tf
@@ -0,0 +1,52 @@
+locals {
+
+  map_worker_roles = [
+    {
+      rolearn : aws_iam_role.node_group.arn
+      username : "system:node:{{EC2PrivateDNSName}}"
+      groups : [
+        "system:bootstrappers",
+        "system:nodes"
+      ]
+    }
+  ]
+}
+
+resource "kubernetes_config_map" "this" {
+  count = var.enable_custom_configmap ? 1 : 0
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+
+    mapRoles    = yamlencode(distinct(concat(local.map_worker_roles, var.map_roles)))
+    mapUsers    = yamlencode(var.map_users)
+    mapAccounts = yamlencode(var.map_accounts)
+  }
+
+}
+
+resource "kubernetes_namespace_v1" "this" {
+  for_each = { for k, v in var.namespaces : k => v }
+
+  metadata {
+    name = each.value.name
+    annotations = {
+      for annotation in each.value.annotations : annotation.label => annotation.value
+    }
+    labels = {
+      for label in each.value.labels : label.label => label.value
+    }
+
+  }
+}
+
+resource "kubernetes_service_account_v1" "this" {
+  for_each = { for k, v in var.service_accounts : k => v }
+  metadata {
+    name      = each.value.name
+    namespace = each.value.namespace
+  }
+}
